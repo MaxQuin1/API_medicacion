ac1974ab9caef50bf58fd964a302b83314ef37bf
MaxQuin1 <maximoquinteroescobar8@gmail.com>
2023-11-03 11:46:51 -0600

    agrego api registro

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const cors = require("cors");
 
 const login = require('./router/loginRouter')
+const agregarUsuario = require('./router/usuarioRouter')
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/login", login);
+app.use("/agregarUsuario", agregarUsuario);
 
 app.listen(8082, () => {
   console.log("servidor iniciando...");
